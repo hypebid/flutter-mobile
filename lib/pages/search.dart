dd064fd03335dc6cee55b1668a96d2255012f0ef
molson82 <wvandebelt82@yahoo.com>
2024-10-20 00:11:13 -0400

    mvp - wip - search page

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hypebid/theme.dart';
 import 'package:hypebid/comp/navigation_bar.dart';
 
 class Search extends StatefulWidget {
@@ -11,19 +12,46 @@ class Search extends StatefulWidget {
 }
 
 class _SearchPageState extends State<Search> {
+  final TextEditingController _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        // child: TwitchLoginBtn(),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Search Page!', style: TextStyle(fontSize: 24))
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: _searchController,
+          decoration: const InputDecoration(
+            hintText: 'Search Streamers',
+            prefixIcon: Icon(Icons.search),
+            filled: true,
+            fillColor: CustomScheme.primaryBackground,
+          ),
         ),
       ),
-      bottomNavigationBar: BtmNavigationBar(navIndex: 1),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return const ListTile(
+            leading: Text(
+              'GW',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: CustomScheme.secondaryText,
+              ),
+            ),
+            title: Text('GiantWaffle'),
+            subtitle: Text('(10.46%) 147.05 hu'),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.arrow_upward, color: CustomScheme.accent4),
+                SizedBox(width: 4),
+                Text('10.46%', style: TextStyle(color: CustomScheme.accent4)),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
